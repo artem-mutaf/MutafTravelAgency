e8f44f52eb6468726170ae4816e6981d914ea5f1
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-11-04 16:42:49 +0200

    add

--- obj/Debug/net8.0/MutafTravelAgency1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MutafTravelAgency1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MutafTravelAgency1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94dd25103337284b3bf72ec480de2c63569542ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9122563c4a113fe1b2494d46c3c96b91e5bcb355")]
 [assembly: System.Reflection.AssemblyProductAttribute("MutafTravelAgency1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MutafTravelAgency1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
